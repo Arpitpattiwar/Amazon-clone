10389d14ddcb289e590cfeb7dec209564c6cb059
Arpit Pattiwar <74522293+Arpitpattiwar@users.noreply.github.com>
2026-02-27 21:09:56 +0530

    Add clothingExtraInfo method to Product and Clothing classes for size chart links

--- data/products.js
+++ data/products.js
@@ -22,6 +22,26 @@ class Product {
   getPrice() {
     return `₨ ${this.price}`;
   }
+
+  clothingExtraInfo() {
+    return '';
+  }
+}
+
+class Clothing extends Product {
+  sizeChartLink;
+
+  constructor(productDetails) {
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  clothingExtraInfo() {
+    return `
+      <a href="${this.sizeChartLink}" target="_blank">
+        Size Chart
+      </a>`
+  }
 }
 
 export const products = [
@@ -684,5 +704,10 @@ export const products = [
     ]
   }
 ].map((productDetails) => {
-  return new Product(productDetails);
+  if (productDetails.type === 'clothing') {
+    return new Clothing(productDetails);
+  }
+  else{
+    return new Product(productDetails);
+  }
 });
