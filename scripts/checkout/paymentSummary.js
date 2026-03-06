9126cad585957ca3fd31beb8e60be55f9d14a40a
Arpit Pattiwar <74522293+Arpitpattiwar@users.noreply.github.com>
2026-03-06 11:49:17 +0530

    Add order management functionality and integrate with order placement

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -1,26 +1,35 @@
 import { cart, getCartArray } from "../../data/cart.js";
 import { products } from "../../data/products.js";
+import { addOrder } from "../../data/orders.js";
 
 if (document.querySelector('.js-place-order')) { enableOrderBtn(); }
 
 function enableOrderBtn() {
-	document.querySelector('.js-place-order').addEventListener('click', async () => {
-	let cartArray = getCartArray();
+	document.querySelector('.js-place-order')
+		.addEventListener('click', async () => {
+			try {
+				let cartArray = getCartArray();
 
-	const response = await fetch('https://supersimplebackend.dev/orders', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json' 
-			},
-			body: JSON.stringify({
-				cart: cartArray
-			})
+				const response = await fetch('https://supersimplebackend.dev/orders', {
+						method: 'POST',
+						headers: {
+							'Content-Type': 'application/json' 
+						},
+						body: JSON.stringify({
+							cart: cartArray
+						})
+					})
+
+				const order = await response.json();
+
+				addOrder(order);
+
+				window.location.href = 'orders.html';
+				
+			} catch (error) {
+				alert('Unexpected error while placing order!')
+			}
 		})
-
-	const order = await response.json();
-
-	console.log(order)
-})
 }
 
 export function calcCartCost() {
